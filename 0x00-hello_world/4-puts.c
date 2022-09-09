d86f2840a220123135aec99eb143a4656b227b2a
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-08 17:31:23 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Use puts function instead of printf
- * The puts function prints an output followed by a new line
- * Unless used with the sytanx fputs (strings,stdout)
- * Return: printf prints a long output with values of variables
+ *
+ *
+ * Return: printf prints a long output wiyh valuesof variables
  */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
