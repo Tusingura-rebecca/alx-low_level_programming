3d2f9a78f9a7d6ebf9b0b9a0696a6e6b86059eea
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-08 19:48:30 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - Use printf instead of puts
-* 
-*Return: printf prints the long output
+*main - print output using printf
+*usng escape characters
+*Return: printf prints long output
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
