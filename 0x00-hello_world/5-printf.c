ddc4c85bf4f5f8add5bff2ae6049304978e81eb2
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-08 17:03:47 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Use printf function
+ *
+ *
+ * Return: printf prints a long output with values of variables
+ */
+int main (void)
+{
+	printf("\nwith proper grammar, but the outcome is a piece of art");
+	return (0);
+}	
