6b537b3dc3b23dad9e1882d0b03e66cc29a84856
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-08 18:37:00 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
-*main - use puts instead of printf
-*
-*
-*Return: printf prints a long output
-*/
+ *main - use puts instead of printf
+ *
+ *Return: printf prints a long output
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
