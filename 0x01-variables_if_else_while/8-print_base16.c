01111332674f13c36cdbd771a8210633cbbabd5e
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-10 12:13:46 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - prints numbers of base 16 in lowetcase using putchar
+*
+*Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9';c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
