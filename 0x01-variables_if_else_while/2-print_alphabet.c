69bb92412781a6105014578b50cc9eef0b793ff5
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-10 10:14:14 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - main block
+*Description: prints the alphabet in loer case
+*and if it prints tha alphabet the right way, then
+*Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c<= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
