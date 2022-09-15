bc900a147762772536d93099e1acc68030305254
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-15 07:24:44 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for prototyps
+*void prototypes (void)
+*int prototypes (int)
+*/
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+int _putchar(char);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+
+#endif /* MAIN_H */
