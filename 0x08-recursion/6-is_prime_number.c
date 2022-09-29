bad6939318f425ff4c87c485a48a0b56274bc6c7
Tusingura-rebecca <rtusingura@gmail.com>
2022-09-28 17:49:49 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*is_prime_number - indicates a prime number
+*@n: integer
+*Return: 1 if n is prime number, 0 if otherwise
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (actual_prime (n, n - 1));
+}
+
+/**
+*actual_prime - calculates if num is prime
+*@n: integer
+*@i: counter
+*Return: 1 if n is prime number, 0 if otherwise
+*/
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
