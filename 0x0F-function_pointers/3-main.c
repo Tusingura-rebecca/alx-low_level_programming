216592720561b0562d0d1c9843c55072bb9d031f
Tusingura-rebecca <rtusingura@gmail.com>
2022-10-14 13:07:50 -0700

    task3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
 *main - program that performs simple operations
@@ -6,40 +8,25 @@
 *@argv: array of arguments
 *Return: 0
 */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int a, b, c;
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	o = *argv[2];
-	
-	if ((o == '/' || o == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	c = f(a, b);
+	printf("%d\n", c);
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,7 +41,7 @@ int op_mul(int a, int b)
 */
 int op_div(int a, int b)
 {
-	return (a / b);
+		return (a / b);
 }
 
 /**
@@ -52,5 +52,5 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-	return (a % b);
+		return (a % b);
 }
