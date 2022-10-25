3009b8f3346ef6cead13450ea6c6906309f9d123
Tusingura-rebecca <rtusingura@gmail.com>
2022-10-25 08:18:43 -0700

    task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (temp == NULL || temp->next == NULL)
 			return (NULL);
+
+		temp = temp->next;
 	}
 
 	new->next = temp->next;
